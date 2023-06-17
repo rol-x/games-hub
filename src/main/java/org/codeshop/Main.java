8ee4e6fc13e1dba9d15be2af555a04a6a9fbc3c3
rol-x <pl.charlessummers@gmail.com>
2023-06-17 12:00:42 +0200

    Add statistics and simulation mode

--- src/main/java/org/codeshop/Main.java
+++ src/main/java/org/codeshop/Main.java
@@ -2,12 +2,10 @@ package org.codeshop;
 
 import org.codeshop.poker.Poker;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
   public static void main(String[] args) {
     var poker = new Poker();
-    poker.addComputerPlayer("Steven");
+    poker.addComputerPlayer("Stephen");
     poker.addComputerPlayer("Maria");
     poker.addHumanPlayer("Charles");
     poker.play();
--- src/main/java/org/codeshop/poker/Poker.java
+++ src/main/java/org/codeshop/poker/Poker.java
@@ -1,18 +1,23 @@
 package org.codeshop.poker;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
+import org.codeshop.poker.card.Ranking;
 import org.codeshop.poker.player.ComputerPlayer;
 import org.codeshop.poker.player.HumanPlayer;
 import org.codeshop.poker.player.Player;
 
 public class Poker {
-  public static final int ROUNDS_LIMIT = 2;
+  public static final int ROUNDS_LIMIT = 200;
   private final List<Player> players = new ArrayList<>();
 
   public void play() {
     boolean alive = true;
-    int roundCount = 1;
+    List<Ranking> allRankings = new ArrayList<>();
+    int roundCount = 0;
     do {
       var round = new PokerRound(players);
       round.dealHandToEachPlayer();
@@ -20,15 +25,52 @@ public class Poker {
       round.displayEachPlayersHand();
       var winningHand = round.findBestHand();
       System.out.println("Winning hand: " + winningHand);
+      allRankings.add(winningHand.getRanking());
 
       var winner = round.findWinningPlayer();
       winner.win(100);
       System.out.println("Winner: " + winner);
       round.disposePlayedCards();
       roundCount += 1;
-      if (roundCount > ROUNDS_LIMIT) alive = false;
+      if (roundCount >= ROUNDS_LIMIT) alive = false;
       System.out.println();
     } while (alive);
+    printStatisticsAfterGame(allRankings, roundCount);
+  }
+
+  private static void printStatisticsAfterGame(List<Ranking> allRankings, int roundCount) {
+    System.out.println("Rounds played: " + roundCount);
+    var rankOccurrences = new StringBuilder();
+    for (var ranking : Ranking.values())
+      if (Collections.frequency(allRankings, ranking) > 0)
+        rankOccurrences.append(
+            "%s: %d%n".formatted(ranking, Collections.frequency(allRankings, ranking)));
+    System.out.println(rankOccurrences);
+  }
+
+  public static void simulateHandDealsAndReportStatistics(int numberOfHandDeals) {
+    System.out.println("Simulation running...");
+    Map<Ranking, Integer> rankingsFrequency = new EnumMap<>(Ranking.class);
+    int roundCount = 0;
+    do {
+      var round = new PokerRound(List.of(new ComputerPlayer("Steven")));
+      round.dealHandToEachPlayer();
+      round.rankEachHand();
+      var ranking = round.findBestHand().getRanking();
+
+      if (rankingsFrequency.containsKey(ranking))
+        rankingsFrequency.put(ranking, rankingsFrequency.get(ranking) + 1);
+      else rankingsFrequency.put(ranking, 1);
+
+      round.disposePlayedCards();
+      roundCount += 1;
+    } while (roundCount < numberOfHandDeals);
+    System.out.println("Rounds played: " + roundCount);
+    double totalRoundCount = roundCount;
+    rankingsFrequency.forEach(
+        (ranking, frequency) ->
+            System.out.printf(
+                "%s: %d (%.2f%%)%n", ranking, frequency, 100f * frequency / totalRoundCount));
   }
 
   public void addHumanPlayer(String name) {
